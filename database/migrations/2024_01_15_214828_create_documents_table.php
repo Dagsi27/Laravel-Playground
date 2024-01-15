fa53f7f6b7e15307f6374ba4fa6407c074ffc5c9
Daniel <daniel.slizewski@gmail.com>
2024-01-15 22:56:28 +0100

    refactor: add documents value

--- database/migrations/2024_01_15_214828_create_documents_table.php
+++ database/migrations/2024_01_15_214828_create_documents_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('blob');
+            $table->string('type');
+            $table->boolean('active');
             $table->timestamps();
         });
     }
